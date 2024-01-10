49587271d71103c9f4ab28668b6f74717c09a440
LSVerslas <148771014+LSVerslas@users.noreply.github.com>
2024-01-10 11:17:58 +0200

    Delete Lauryno chat directory

--- Lauryno chat/src/routes/v1/index.js
+++
@@ -1,6 +0,0 @@
-const logs = require('./logs');
-const medications = require('./medications');
-const pets = require('./pets');
-const prescriptions = require('./prescriptions');
-
-module.exports = { logs, medications, pets, prescriptions };
